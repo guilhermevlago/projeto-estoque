112f231a401c27165a426f55dd279ec6605909fd
Guilherme Lago <gvidallago@gmail.com>
2025-07-01 19:40:39 -0300

    #

--- api/routes/login.js
+++ api/routes/login.js
@@ -14,7 +14,7 @@ router.post('/', async (req, res) => {
     if (rows.length === 0) return res.status(401).json({ error: 'Usuário ou senha inválidos.' });
 
     const user = rows[0];
-    
+    //
     const senhaCorreta = senha === user.senha;
 
     if (!senhaCorreta) return res.status(401).json({ error: 'Usuário ou senha inválidos.' });
